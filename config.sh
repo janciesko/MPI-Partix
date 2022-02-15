9ab5d144a16490ceccec8fbb72999f62e3278283
Jan Ciesko <jan.ciesko@gmail.com>
2022-02-15 09:23:10 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-cmake -DCMAKE_C_COMPILER=mpicc -DCMAKE_LINKER=mpicc -DQthreads_ROOT=$QTHREADS_INSTALL_PATH -DPartix_ENABLE_QTHREADS=ON
+
+# Provides an example of how to configure Partix with cmake
+# ---------------------------------------------------------
+
+cd build
+cmake ..  -DCMAKE_C_COMPILER=mpicc -DCMAKE_LINKER=mpicc -DQthreads_ROOT=$QTHREADS_INSTALL_PATH -DPartix_ENABLE_QTHREADS=ON
