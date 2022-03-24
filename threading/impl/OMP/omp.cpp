34b3ef03d45c5a0ff586e577e4bfeb35adc87b8b
Jan Ciesko <jan.ciesko@gmail.com>
2022-03-23 22:23:25 -0600

    Update use of Partix API in OMP backend

--- threading/impl/OMP/omp.cpp
+++ threading/impl/OMP/omp.cpp
@@ -15,9 +15,10 @@
 #include <thread.h>
 
 partix_mutex_t global_mutex;
-partix_config_t * global_conf;
+partix_config_t *global_conf;
 
-void partix_task(void (*f)(partix_task_args_t *), void *args) {
+void partix_task(void (*f)(partix_task_args_t *), void *args,
+                 partix_context_t *ctx) {
 #pragma omp task
   {
     partix_task_args_t partix_args;
@@ -26,39 +27,23 @@ void partix_task(void (*f)(partix_task_args_t *), void *args) {
   }
 }
 
-void partix_taskwait(void) {
+void partix_taskwait(partix_context_t *ctx) {
 #pragma omp taskwait
 }
 
-void partix_mutex_init(partix_mutex_t * m){
-  omp_init_lock(m);
-};
-void partix_mutex_destroy(partix_mutex_t * m){ 
-  omp_destroy_lock(m);
-};
+void partix_mutex_init(partix_mutex_t *m) { omp_init_lock(m); };
+void partix_mutex_destroy(partix_mutex_t *m) { omp_destroy_lock(m); };
 
-void partix_mutex_enter(partix_mutex_t * m) { 
-  omp_set_lock(m);  
-};
+void partix_mutex_enter(partix_mutex_t *m) { omp_set_lock(m); };
 
-void partix_mutex_exit(partix_mutex_t * m) { 
-  omp_unset_lock(m);
-};
+void partix_mutex_exit(partix_mutex_t *m) { omp_unset_lock(m); };
 
-void partix_mutex_init(){
-  omp_init_lock(&global_mutex);
-};
-void partix_mutex_destroy(){ 
-  omp_destroy_lock(&global_mutex);
-};
+void partix_mutex_init() { omp_init_lock(&global_mutex); };
+void partix_mutex_destroy() { omp_destroy_lock(&global_mutex); };
 
-void partix_mutex_enter(void) { 
-  omp_set_lock(&global_mutex);  
-};
+void partix_mutex_enter(void) { omp_set_lock(&global_mutex); };
 
-void partix_mutex_exit(void) { 
-  omp_unset_lock(&global_mutex);
-};
+void partix_mutex_exit(void) { omp_unset_lock(&global_mutex); };
 
 int partix_executor_id(void) { return omp_get_thread_num(); };
 
